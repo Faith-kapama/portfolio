095b27b3030878afa39e0d9abe3c887f0c7522dd
Faith-kapama <faithkapama@gmail.com>
2026-09-01 20:38:20 +0300

    modified portfolio

--- backend/portfolio_api/settings.py
+++ backend/portfolio_api/settings.py
@@ -79,11 +79,11 @@ WSGI_APPLICATION = 'portfolio_api.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'portfolio_db',
-        'USER': 'faith',
-        'PASSWORD': 'nyamtondo',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST')
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
